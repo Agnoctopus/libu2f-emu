0399335ea7473704cfe1882cc0c816001a8b1b6e
Cesar "MattGorko" Belley <cesar.belley@epita.fr>
2020-05-19 15:49:24 +0200

    [EMU] Add input process function for devices
    Signed-off-by: Cesar "MattGorko" Belley <cesar.belley@epita.fr>

--- src/u2f-emu.c
+++ src/u2f-emu.c
@@ -4,6 +4,30 @@
 
 
 /**
+** \brief Input handler for U2F virtual emulated device input
+**        processing.
+*/
+typedef void (*input_handler_t)(void *data, size_t size);
+
+/**
+** \brief Input handlers for U2F virtual emulated device input
+**        processing, depending of the associated transport.
+*/
+static const input_handler_t input_handlers[] =
+{
+    NULL,
+    NULL,
+    NULL,
+    NULL
+};
+
+/**
+** \brief Number of inputs handlers
+*/
+static const size_t input_handlers_nb =
+        sizeof(input_handlers) / sizeof(input_handlers[0]);
+
+/**
 ** \brief U2F virtual emulated device
 */
 struct u2f_emu_vdev
@@ -11,6 +35,50 @@ struct u2f_emu_vdev
     u2f_emu_transport transport; /**< The underlaying transport */
     u2f_emu_apdu apdu; /**< The apdu format */
 };
+
+
+u2f_emu_rc u2f_emu_vdev_process(u2f_emu_vdev *vdev,
+        void *data, size_t size)
+{
+    /* Input handler */
+    input_handler_t input_handler;
+
+    /* Transport existance */
+    if (vdev->transport < 0 || vdev->transport >= input_handlers_nb)
+        return U2F_EMU_SUPPORTED_ERROR;
+
+    /* Get the input handler corresponding to the transport */
+    input_handler = input_handlers[vdev->transport];
+
+    /* Check for implementation */
+    if (input_handlers == NULL)
+        return U2F_EMU_SUPPORTED_ERROR;
+
+    /* Process input */
+    input_handler(data, size);
+
+    return U2F_EMU_OK;
+}
+
+u2f_emu_rc u2f_emu_vdev_set_apdu(u2f_emu_vdev *vdev,
+    u2f_emu_apdu apdu)
+{
+    /* Check permissions */
+    if (vdev->transport != U2F_EMU_NFC
+        && vdev->transport != U2F_EMU_AGNOSTIC)
+        return U2F_EMU_PERMISSION_ERROR;
+
+    /* Set apdu */
+    vdev->apdu = apdu;
+
+    return U2F_EMU_OK;
+}
+
+void u2f_emu_vdev_free(u2f_emu_vdev *vdev)
+{
+    /* Release */
+    free(vdev);
+}
 
 u2f_emu_rc u2f_emu_vdev_new(u2f_emu_vdev **vdev_ref,
         u2f_emu_transport transport)
@@ -35,25 +103,4 @@ u2f_emu_rc u2f_emu_vdev_new(u2f_emu_vdev **vdev_ref,
     *vdev_ref = vdev;
 
     return U2F_EMU_OK;
-}
-
-u2f_emu_rc u2f_emu_vdev_set_apdu(u2f_emu_vdev *vdev,
-    u2f_emu_apdu apdu)
-{
-    /* Check permissions */
-    if (vdev->transport != U2F_EMU_NFC
-        && vdev->transport != U2F_EMU_AGNOSTIC)
-        return U2F_EMU_PERMISSION_ERROR;
-
-    /* Set apdu */
-    vdev->apdu = apdu;
-
-    return U2F_EMU_OK;
-}
-
-
-void u2f_emu_vdev_free(u2f_emu_vdev *vdev)
-{
-    /* Release */
-    free(vdev);
 }
--- src/u2f-emu.h
+++ src/u2f-emu.h
@@ -1,6 +1,9 @@
 #ifndef U2F_EMU
 #define U2F_EMU
 
+#include <stddef.h>
+
+
 #include "u2f-emu-types.h"
 
 
@@ -13,32 +16,45 @@
 const char *u2f_emu_strerror(u2f_emu_rc rc);
 
 /**
-** \brief Instantiate a new U2F virtual emultaed device.
+** \brief Instantiate a new U2F virtual emultated device.
 **
 ** \param vdev Virtual device reference.
 ** \return Success: U2F_EMU_OK.
 **         Failure: - memory allocation: U2F_EMU_MEMORY_ERROR.
-**                  - not supported: U2F_EMU_SUPPORTED_ERROR
+**                  - not supported: U2F_EMU_SUPPORTED_ERROR.
 */
 u2f_emu_rc u2f_emu_vdev_new(u2f_emu_vdev **vdev,
         u2f_emu_transport transport);
 
 /**
-** \brief Set the apdu length of an U2F virtual emultaed device.
+** \brief Set the apdu length of an U2F virtual emultated device.
 **
 ** \param vdev Virtual device pointer.
-** \param apdu The apdu length
-** \return Success: U2F_EMU_OK
+** \param apdu The apdu length.
+** \return Success: U2F_EMU_OK.
 **         Failure: not allowed: U2F_EMU_PERMISSION_ERROR
 */
 u2f_emu_rc u2f_emu_vdev_set_apdu(u2f_emu_vdev *vdev_ref,
-    u2f_emu_apdu apdu);
+        u2f_emu_apdu apdu);
 
 /**
-** \brief Free an U2F virtual emultaed device.
+** \brief Free an U2F virtual emultated device.
 **
-** \param vdev Virtual device pointer
+** \param vdev Virtual device pointer.
 */
 void u2f_emu_vdev_free(u2f_emu_vdev *vdev);
+
+/**
+** \brief Ask an U2F virtual emultaed device to process input data.
+**
+** \param vdev Virtual device pointer.
+** \param data The input data to process.
+** \param size The size of the input data in bytes.
+** \return Success: U2F_EMU_OK.
+**         Failure: - transport does not exist, or is not
+**                    implemented: U2F_EMU_SUPPORTED_ERROR:.
+*/
+u2f_emu_rc u2f_emu_vdev_process(u2f_emu_vdev *vdev,
+        void *data, size_t size);
 
 #endif
