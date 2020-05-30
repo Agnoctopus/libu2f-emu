2946b095b0226205ce2f81f9cd6d4232e188312c
Cesar "MattGorko" Belley <cesar.belley@epita.fr>
2020-05-30 13:50:06 +0200

    [EMU] Clarify API and add doc
    Signed-off-by: Cesar "MattGorko" Belley <cesar.belley@epita.fr>

--- src/u2f-emu.h
+++ src/u2f-emu.h
@@ -1,8 +1,9 @@
 #ifndef U2F_EMU
 #define U2F_EMU
 
+#include <stdbool.h>
 #include <stddef.h>
-
+#include <stdint.h>
 
 #include "u2f-emu-types.h"
 
@@ -18,7 +19,7 @@ const char *u2f_emu_strerror(u2f_emu_rc rc);
 /**
 ** \brief Instantiate a new U2F virtual emulated device.
 **
-** \param vdev_ref Virtual device reference.
+** \param vdev_ref The virtual device reference.
 ** \param transport The transport of the virtual device.
 ** \return Success: U2F_EMU_OK.
 **         Failure: - memory allocation: U2F_EMU_MEMORY_ERROR.
@@ -30,7 +31,7 @@ u2f_emu_rc u2f_emu_vdev_new(u2f_emu_vdev **vdev_ref,
 /**
 ** \brief Set the apdu length of an U2F virtual emultated device.
 **
-** \param vdev Virtual device pointer.
+** \param vdev The virtual device.
 ** \param apdu The apdu length.
 ** \return Success: U2F_EMU_OK.
 **         Failure: not allowed: U2F_EMU_PERMISSION_ERROR
@@ -41,14 +42,14 @@ u2f_emu_rc u2f_emu_vdev_set_apdu(u2f_emu_vdev *vdev,
 /**
 ** \brief Free an U2F virtual emulated device.
 **
-** \param vdev Virtual device pointer.
+** \param vdev The virtual device.
 */
 void u2f_emu_vdev_free(u2f_emu_vdev *vdev);
 
 /**
 ** \brief Ask an U2F virtual emulated device to process input data.
 **
-** \param vdev Virtual device pointer.
+** \param vdev The virtual device.
 ** \param data The input data to process.
 ** \param size The size of the input data in bytes.
 ** \return Success: U2F_EMU_OK.
@@ -57,5 +58,24 @@ void u2f_emu_vdev_free(u2f_emu_vdev *vdev);
 */
 u2f_emu_rc u2f_emu_vdev_process(u2f_emu_vdev *vdev,
         const void *data, size_t size);
+
+/**
+** \brief Check if an U2F virtual emulated device has response data
+**        To previous processed data.
+**
+** \param vdev The virtual device pointer.
+** \return Has response: true.
+**         Has not response: false.
+*/
+bool u2f_emu_vdev_has_response(u2f_emu_vdev *vdev);
+
+/**
+** \brief Get a response from an U2F virtual emulated device.
+**
+** \param vdev The virtual device pointer.
+** \param data The data ref buffer to put the response.
+** \return The size of the allocated data.
+*/
+size_t u2f_emu_vdev_get_response(u2f_emu_vdev *vdev, uint8_t **data);
 
 #endif
