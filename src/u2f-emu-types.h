c9ae68dffde3ed33cd33583f5427154d9aa2cebb
Cesar "MattGorko" Belley <cesar.belley@epita.fr>
2020-06-07 20:01:58 +0200

    [EMU] Add doc, setup struct and more generic counter
    Signed-off-by: Cesar "MattGorko" Belley <cesar.belley@epita.fr>

--- src/u2f-emu-types.h
+++ src/u2f-emu-types.h
@@ -1,6 +1,12 @@
 #ifndef U2F_EMU_TYPES_H
 #define U2F_EMU_TYPES_H
 
+#include <stdint.h>
+
+
+/**
+** \brief U2F-emu return code.
+*/
 typedef enum
 {
   U2F_EMU_OK = 0,
@@ -10,6 +16,9 @@ typedef enum
   U2F_EMU_SUPPORTED_ERROR
 } u2f_emu_rc;
 
+/**
+** \brief U2F-emu transport emulation.
+*/
 typedef enum
 {
   U2F_EMU_APDU = 0,
@@ -18,12 +27,39 @@ typedef enum
   U2F_EMU_NFC
 } u2f_emu_transport;
 
+/**
+** \brief U2F-emu APDU format length.
+*/
 typedef enum
 {
     U2F_EMU_EXTENDED = 0,
     U2F_EMU_SHORT,
 } u2f_emu_apdu;
 
+/**
+** \brief U2F virtual emulated device counter structure.
+*/
+struct u2f_emu_vdev_counter {
+  /* Get counter value */
+	uint32_t (*counter_read)(struct u2f_emu_vdev_counter *counter);
+  /* Increment counter value */
+	void (*counter_increment)(struct u2f_emu_vdev_counter *counter);
+};
+
+/**
+** \brief U2F virtual emulated device setup structure, containing all
+**        necessary elements to the device setup.
+*/
+struct u2f_emu_vdev_setup {
+	uint8_t *certificate; /**< PEM ec certificate */
+	uint8_t *private_key; /**< PEM ec Private key of the certificate */
+	uint8_t entropy[48]; /**< Random bits used in encryption */
+  struct u2f_emu_vdev_counter *counter; /**< Counter */
+};
+
+/**
+** \brief Typedef forward declaration of u2f_emu_vdev.
+*/
 typedef struct u2f_emu_vdev u2f_emu_vdev;
 
 #endif
