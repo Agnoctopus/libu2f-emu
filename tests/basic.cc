12606bd5c7c078f2f700ee2f6be955ed21ce42f6
Cesar "MattGorko" Belley <cesar.belley@epita.fr>
2020-06-10 14:25:46 +0200

    [TESTS] Fix all tests with the new API
    Signed-off-by: Cesar "MattGorko" Belley <cesar.belley@epita.fr>

--- tests/basic.cc
+++ tests/basic.cc
@@ -8,14 +8,13 @@ extern "C" {
 #define SETUP_DIR ".u2f.tmp"
 
 
-TEST(USB, BasicNew)
+TEST(SetupDir, BasicNew)
 {
     /* Given */
     u2f_emu_vdev *vdev = NULL;
 
     /* When */
-    u2f_emu_rc rc = u2f_emu_vdev_new_from_dir(&vdev,
-            U2F_EMU_USB, SETUP_DIR);
+    u2f_emu_rc rc = u2f_emu_vdev_new_from_dir(&vdev, SETUP_DIR);
 
     /* Then */
     EXPECT_EQ(rc, U2F_EMU_OK);
@@ -25,104 +24,17 @@ TEST(USB, BasicNew)
     u2f_emu_vdev_free(vdev);
 }
 
-TEST(USBWrongSetupDir, BasicNew)
+TEST(WrongSetupDir, BasicNew)
 {
     /* Given */
     u2f_emu_vdev *vdev = NULL;
 
     /* When */
-    u2f_emu_rc rc = u2f_emu_vdev_new_from_dir(&vdev,
-            U2F_EMU_USB, "nop");
+    u2f_emu_rc rc = u2f_emu_vdev_new_from_dir(&vdev, "nop");
 
     /* Then */
     EXPECT_EQ(rc, U2F_EMU_MEMORY_ERROR);
     EXPECT_EQ(vdev, nullptr);
-
-    /* After */
-    u2f_emu_vdev_free(vdev);
-}
-
-TEST(NFC, BasicNew)
-{
-    /* Given */
-    u2f_emu_vdev *vdev = NULL;
-
-    /* When */
-    u2f_emu_rc rc = u2f_emu_vdev_new_from_dir(&vdev,
-            U2F_EMU_NFC, SETUP_DIR);
-
-    /* Then */
-    EXPECT_EQ(rc, U2F_EMU_SUPPORTED_ERROR);
-    EXPECT_EQ(vdev, nullptr);
-
-    /* After */
-    u2f_emu_vdev_free(vdev);
-}
-
-TEST(BL, BasicNew)
-{
-    /* Given */
-    u2f_emu_vdev *vdev = NULL;
-
-    /* When */
-    u2f_emu_rc rc = u2f_emu_vdev_new_from_dir(&vdev,
-            U2F_EMU_BLUETOOTH, SETUP_DIR);
-
-    /* Then */
-    EXPECT_EQ(rc, U2F_EMU_SUPPORTED_ERROR);
-    EXPECT_EQ(vdev, nullptr);
-
-    /* After */
-    u2f_emu_vdev_free(vdev);
-}
-
-TEST(Agnostic, BasicNew)
-{
-    /* Given */
-    u2f_emu_vdev *vdev = NULL;
-
-    /* When */
-    u2f_emu_rc rc = u2f_emu_vdev_new_from_dir(&vdev,
-            U2F_EMU_APDU, SETUP_DIR);
-
-    /* Then */
-    EXPECT_EQ(rc, U2F_EMU_SUPPORTED_ERROR);
-    EXPECT_EQ(vdev, nullptr);
-
-    /* After */
-    u2f_emu_vdev_free(vdev);
-}
-
-
-TEST(WrongTransport, BasicNew)
-{
-    /* Given */
-    u2f_emu_vdev *vdev = NULL;
-
-    /* When */
-    u2f_emu_rc rc = u2f_emu_vdev_new_from_dir(&vdev,
-            (u2f_emu_transport) - 1, SETUP_DIR);
-
-    /* Then */
-    EXPECT_EQ(rc, U2F_EMU_SUPPORTED_ERROR);
-    EXPECT_EQ(vdev, nullptr);
-
-    /* After */
-    u2f_emu_vdev_free(vdev);
-}
-
-TEST(BasicUSB, BasicAPDU)
-{
-    /* Given */
-    u2f_emu_vdev *vdev = NULL;
-    u2f_emu_vdev_new_from_dir(&vdev, U2F_EMU_USB, SETUP_DIR);
-
-    /* When */
-    u2f_emu_rc rc = u2f_emu_vdev_set_apdu(vdev, U2F_EMU_USB,
-            U2F_EMU_EXTENDED);
-
-    /* Then */
-    EXPECT_EQ(rc, U2F_EMU_PERMISSION_ERROR);
 
     /* After */
     u2f_emu_vdev_free(vdev);
